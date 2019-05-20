54f17d35d21f6f608641b656b9cdc23b52e92002
p.kabir <p.kabir@protoware.com>
2019-05-20 17:26:48 +0300

    Add comment

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 module.exports = {
     parser: '@typescript-eslint/parser',
     parserOptions: {
+        // Override this in your .eslintrc.js if needed.
+        // -
+        project: 'tsconfig.json',
         ecmaVersion: 2018,
         sourceType: 'module',
         ecmaFeatures: {
